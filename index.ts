752e699ee1ba1413a36255e61762c5b94e6aff9a
Tyson Adams <tadams@squiz.net>
2021-12-01 16:18:06 +1000

    Problem 2

--- index.ts
+++ index.ts
@@ -3,14 +3,31 @@ import { readFileSync } from 'fs';
 const text = readFileSync("./input.txt", 'utf-8');
 const depthsArray = text.split("\n")
 
-let previousMeasure = 0;
-let countOfIncreases = 0;
-depthsArray.forEach(depth => {
-    let numberDepth = parseInt(depth);
-    if (previousMeasure !== 0 && numberDepth > previousMeasure) {
-        countOfIncreases++;
-    }
-    previousMeasure = numberDepth;
-});
+const calculateIncreases = (input: string[]) => {
+    let previousMeasure = 0;
+    let countOfIncreases = 0;
+    input.forEach((depth: string) => {
+        const depthNum = parseInt(depth);
+        if (previousMeasure !== 0 && depthNum > previousMeasure) {
+            countOfIncreases++;
+        }
+        previousMeasure = depthNum;
+    });
+    return countOfIncreases;
+}
 
-console.log(countOfIncreases);
+const calculateSlidingIncreases = (input: string[]) => {
+    let bucket: string[] = [];
+    depthsArray.forEach((depth: string, index: number) => {
+        let numberDepth = parseInt(depth);
+        let nextDepth = parseInt(depthsArray[index + 1]);
+        let finalDepth = parseInt(depthsArray[index + 2]);
+        bucket.push(String(numberDepth + nextDepth + finalDepth));
+    });
+    return calculateIncreases(bucket);
+}
+
+// Problem 1
+console.log(calculateIncreases(depthsArray));
+// Problem 2
+console.log(calculateSlidingIncreases(depthsArray));
